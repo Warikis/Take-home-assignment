e8d55db3073ab6c909551c4cc1583d265f5d54f6
Denys Yushanov <yushanovj@gmail.com>
2024-07-13 07:22:36 +0300

    Changed views to generic one

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -4,13 +4,8 @@ from . import views
 
 app_name = "polls"
 urlpatterns = [
-    path("", views.index, name="index"),
-    #path for questions
-    # the 'name' value as called by the {% url %} template tag
-    # added the word 'specifics'
-    path("<int:question_id>/", views.detail, name="detail"),
-    #path for results
-    path("<int:question_id>/results/", views.results, name="results"),
-    #path for votes
+    path("", views.IndexView.as_view(), name="index"),
+    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
+    path("<int:pk>/results", views.ResultsView.as_view(), name="results"),
     path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
