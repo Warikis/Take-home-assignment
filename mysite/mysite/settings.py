119ec1bfba89d9c33d879b90f8009a4c5ffb0748
Denys Yushanov <yushanovj@gmail.com>
2024-07-14 07:48:31 +0300

    Added React and API endpoint

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'rest_framework',
     'corsheaders',
+    'myapi.apps.MyapiConfig',
     'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
